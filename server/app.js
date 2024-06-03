509dcfd50cfd970d5854d6e7f67aeb4ee08b564c
MJ20027 <mi02code@gmail.com>
2024-06-03 16:49:51 +0530

    url commit

--- server/app.js
+++ server/app.js
@@ -43,6 +43,13 @@ app.use(cors());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 
 app.use("/api", authRouter);
 app.use("/api/user", usersRouter);
